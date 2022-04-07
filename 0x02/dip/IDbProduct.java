db84507dceaa430ea0046f0a30b18ef18fa1c8a3
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-07 15:12:53 -0300

    dip

---
+++ 0x02/dip/IDbProduct.java
@@ -0,0 +1,8 @@
+
+public interface IDbProduct {
+
+	 String getProductById(String productID);
+	  String getAll();
+	  String update(Product product);
+	  String delete(String productID);
+}
---
+++ 0x02/dip/Product.java
@@ -0,0 +1,9 @@
+
+public class Product {
+
+	private Long id;
+	private String name;
+	
+	
+	
+}
