2f724721232ba602836813fb8883d543a4524df8
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-06 23:30:43 -0300

    ocp

---
+++ 0x01/ocp/IVehicleMotorcycle.java
@@ -0,0 +1,6 @@
+public interface IVehicleMotorcycle {
+
+	 void configureVehicle(String color, String year, double engine);
+
+     void startVehicle();
+}
---
+++ 0x01/ocp/Motorcycle.java
@@ -0,0 +1,24 @@
+public class Motorcycle extends Vehicle implements IVehicleMotorcycle   {
+
+	public Motorcycle(String color, String year, double engine) {
+		super(color, year, engine);
+		configureVehicle(color, year, engine);
+		
+	}
+
+	@Override
+	public void configureVehicle(String color, String year, double engine) {
+		System.out.println("Criando uma moto: " + year + " com " + engine + " cilindradas");
+		startVehicle();
+		
+	}
+	@Override
+	public void startVehicle() {
+		System.out.println("Ligando os motores");
+	}
+	}
+
+
+	
+
+
