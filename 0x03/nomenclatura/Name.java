ccbba831737fe7e85f178ea6d3e6c0b7d096fed7
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-08 12:00:50 -0300

    0x03 - nomenclatura

---
+++ 0x03/nomenclatura/Name.java
@@ -0,0 +1,33 @@
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+public class Name {
+
+	public Date d;
+
+	public List<Testcase> find(Name.User user) {
+	};
+
+	public Account[] AccountList;
+
+	public void deleteUser(Name.User user) {
+	};
+
+	public URL url;
+
+	public class TableRepresenter {
+	};
+
+	public void car() {
+	};
+
+	public saveUser(){};
+
+	createAccount(){};
+
+	generateContract(){};
+
+	public final int DAYS_IN_YEAR = 360;
+	public final int HOURS_IN_WEEK = 168;
+}
