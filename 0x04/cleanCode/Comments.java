b57ee6523a2ee3bf0473c73d1b3344070c341718
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-12 19:27:10 -0300

    cleanCode

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,36 @@
+package cleanCode;
+
+public class Comments {
+
+
+
+
+    // this method delivers owner of testcase
+    public Owner getTestcaseOwner(Testcase testcase){}; 
+
+
+    // returns hours in month
+    public Hours getHoursInQuarter(){}; 
+
+
+    //public void checkXMLParser(){
+    //   // some business logic here
+    //}
+
+    // TODO Este métod deve ser removido
+    public void check(){
+        return;
+    }
+
+
+    // 3. Too much information
+    /*
+     * This class evaluates the overview of testcases which are cancelled from client
+     * the reasons of cancellation play a role in evaluation blablabla
+     */
+
+
+    public List<Result> publishResults(Date date) {
+        // some business logic here
+    }
+}
