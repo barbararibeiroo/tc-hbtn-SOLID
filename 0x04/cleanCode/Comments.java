0803ec7c84917dd012629704df1f5c6d3ac3ec95
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-12 19:28:37 -0300

    cleanCode

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -2,35 +2,17 @@ package cleanCode;
 
 public class Comments {
 
+	public Owner getTestcaseOwner(Testcase testcase) {
+	};
 
+	public Hours getHoursInQuarter() {
+	};
 
+	public void check() {
+		return;
+	}
 
-    // this method delivers owner of testcase
-    public Owner getTestcaseOwner(Testcase testcase){}; 
+	public List<Result> publishResults(Date date) {
 
-
-    // returns hours in month
-    public Hours getHoursInQuarter(){}; 
-
-
-    //public void checkXMLParser(){
-    //   // some business logic here
-    //}
-
-    // TODO Este métod deve ser removido
-    public void check(){
-        return;
-    }
-
-
-    // 3. Too much information
-    /*
-     * This class evaluates the overview of testcases which are cancelled from client
-     * the reasons of cancellation play a role in evaluation blablabla
-     */
-
-
-    public List<Result> publishResults(Date date) {
-        // some business logic here
-    }
+	}
 }
