f13b82bb1071dd251a7d65b6efc4d757026bbf42
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-12 20:13:52 -0300

    convencao

--- 0x04/convencao/User.java
+++ 0x04/convencao/User.java
@@ -1,14 +1,23 @@
 
+
 public class User {
 
-	int IdUser;
+int IdUser;
 
-	String Descricao;
-	int IDADEMINIMA = 18;
+String Descricao;
 
+final static int IDADEMINIMA = 18;
 
-	public AllUsers() {return [];}
+// função que coleta todos os users
 
+public FetchUsers() { return [];}
 
+// função que coleta todos os users
+
+public AllUsers() {return [];}
+
+// função que coleta todos os users
+
+public GetUsers() {return [];}
 
 }
