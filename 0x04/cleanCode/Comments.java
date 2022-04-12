de9499600a0a6905c64bc924f57842abdc8aa8a2
barbararibeiroo <barbararibeiro18@gmail.com>
2022-04-12 19:29:31 -0300

    cleanCode

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -11,7 +11,13 @@ public class Comments {
 	public void check() {
 		return;
 	}
-
+    /**
+     * <p> this method publishes results based on date
+     * </p>
+     * @param date date from when results should be published
+     * @return List of Result entities
+     * @since 16.0
+     */
 	public List<Result> publishResults(Date date) {
 
 	}
